b49b29386bb590bbb88d33bd65e135d6adee4f9b
malsvet <47292391+malsvet@users.noreply.github.com>
2022-10-19 00:53:58 +0700

    Add files via upload

---
+++ Max/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа запрашивает у пользователя три числа и выводит на экран максимальное число.
+
+Console.Write("Введите первое число: ");
+double a = Double.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double b = Double.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+double c = Double.Parse(Console.ReadLine());
+double max;
+if (a > b) max = a;
+else max = b;
+if (c > max) max = c;
+Console.Write($"Максимальное число: {max}");
+Console.ReadKey();
+
+
